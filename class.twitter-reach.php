58933a5b5e084e8e5ab6676a690bfdf8a6511803
Benjamin J. Balter <ben@balter.com>
2012-03-05 13:27:12 -0500

    comments

--- class.twitter-reach.php
+++ class.twitter-reach.php
@@ -1,15 +1,34 @@
 <?php
-
+/**
+ * Given a twitter search query, calculates the a rough number representing the direct reach
+ * 
+ * Caveats:
+ * 
+ * 1) Doesn't take into account the fact that a given user may follow two people that tweet a URL
+ *     e.g., just adds follower counts of all matching tweets
+ *
+ * 2) Doesn't take into acount RTs
+ *     e.g., if a tweet mentions the query parameter, and then is RT'd, the RT will not be counted
+ * 
+ * 3) Limited to first 100 results (no paging)
+ *
+ * @author Benjamin J. Balter
+ * @license GPL3
+ * @version 1.0
+ */
 class Twitter_Reach {
 
-	public $tweets = array();
-	public $users = array();
-	public $follower_counts = array();
+	public $tweets = array(); //stores tweet data (as $this->tweets->results)
+	public $users = array(); //stores user data
+	public $follower_counts = array(); //array in form of screen_name => follower count
 	public $embed_code = '<blockquote class="twitter-tweet"><p>%1$s</p>&mdash; %2$s (@%3$s) <a href="https://twitter.com/twitterapi/status/%4$d" data-datetime="%5$s">%6$s</a></blockquote>';
 	public $search_base = 'http://search.twitter.com/search.json?rpp=100&q=%s';
 	public $user_lookup_base = 'http://api.twitter.com/1/users/lookup.json?include_entities=true&screen_name=%s';
-	public $reach;
+	public $reach; // (int) total reach
 	
+	/**
+	 * The class's main action
+	 */
 	function __construct( $query = null ) {
 		
 		if ( $query == null )
@@ -22,6 +41,9 @@ class Twitter_Reach {
 		
 	}
 	
+	/**
+	 * Gets a URL and JSON decodes it
+	 */
 	function get_and_decode( $url ) {
 	
 		$data = file_get_contents( $url );
@@ -38,6 +60,9 @@ class Twitter_Reach {
 
 	}
 	
+	/**
+	 * Retrieve array of tweet objects for given query
+	 */
 	function get_tweets( $q ) {
 	
 		$url = sprintf( $this->search_base, urlencode( $q ) );
@@ -46,6 +71,9 @@ class Twitter_Reach {
 		
 	}
 	
+	/**
+	 * Propegates empty follower count array from given set of tweets
+	 */
 	function build_follower_array( $data = null ) {
 		
 		if ( $data == null )
@@ -58,6 +86,9 @@ class Twitter_Reach {
 		
 	}
 	
+	/**
+	 * Retrieve extended user data for a set of users
+	 */
 	function get_users( $users = array() ) {
 		
 		if ( empty( $users ) )
@@ -72,6 +103,9 @@ class Twitter_Reach {
 		
 	}
 	
+	/**
+	 * Push follower counts into follower_count array and into tweets so we can sort
+	 */
 	function propegate_follower_counts( $users = array(), $tweets = array() ) {
 		
 		if ( empty( $users ) )
@@ -94,18 +128,28 @@ class Twitter_Reach {
 		
 	}
 	
+	/**
+	 * Render tweet in blackbird pie compatible format
+	 */
 	function render_tweet( $tweet ) {		
 			
 		return sprintf( $this->embed_code, $tweet->text, $tweet->from_user_name, $tweet->from_user, $tweet->id, $tweet->created_at, date( 'F j, Y', strtotime( $tweet->created_at ) ) );
 		
 	}
 	
+	/**
+	 * Helper function to sort objects by property
+	 * source: http://stackoverflow.com/questions/124266/sort-object-in-php
+	 */
 	function compare( $a, $b ) { 
 	  if( $a->reach == $b->reach ){ return 0 ; } 
 	  return ($a->reach > $b->reach ) ? -1 : 1;
 	} 
-	
-	//http://stackoverflow.com/questions/124266/sort-object-in-php
+
+	/**
+	 * Sorts tweets by reach DESC
+	 * source: http://stackoverflow.com/questions/124266/sort-object-in-php
+	 */
 	function sort( &$array ) {
 		usort( $array, array( &$this, 'compare' ) );
 	}
